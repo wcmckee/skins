f53c4dde778535d37113ccf3e5e4cb51755b27d8
wcmckee <hammersmake@gmail.com>
2013-12-10 13:12:17 +1300

    updated py file

--- skins.py
+++ skins.py
@@ -10,30 +10,32 @@ from PIL import ImageFilter
 import random
 import os
 import sys
+import glob
 
 # <codecell>
 
-os.chdir('/home/will/Desktop/skins')
+devInput = ('/home/will/Desktop/video/street')
 
 # <codecell>
 
-imagRancz = random.choice(os.listdir('/home/will/Desktop/skins'))
+os.chdir('/home/will/Desktop/video/street')
+
+# <codecell>
+
+imagRancz = random.choice(os.listdir(devInput))
 
 # <codecell>
 
 
-imagRandz = random.choice(os.listdir('/home/will/Desktop/skins'))
+imagRandz = random.choice(os.listdir(devInput))
 
 # <codecell>
 
-imagRanxz = random.choice(os.listdir('/home/will/Desktop/skins'))
+imagRanxz = random.choice(os.listdir(devInput))
 
 # <codecell>
 
-imagCook = random.choice(os.listdir('/home/will/Desktop/cook'))
-
-# <codecell>
-
+imagCook = random.choice(os.listdir(devInput))
 
 # <codecell>
 
@@ -59,39 +61,6 @@ iimg5 = Image.open(imagRancz)
 
 # <codecell>
 
-iimg5.show()
-
-# <codecell>
-
-effectList = ['difference', 'darker', 'lighter', 'multiply', 'screen']
-
-# <codecell>
-
-lenEff = len(effectList)
-
-# <codecell>
-
-print lenEff
-
-# <codecell>
-
-daChoice = random.choice(effectList)
-
-# <codecell>
-
-print daChoice
-
-# <codecell>
-
-mergeChop = 'ImageChops.' + daChoice
-
-# <codecell>
-
-print mergeChop
-
-# <markdowncell>
-
-# apply this effect to the files
 
 # <codecell>
 
@@ -103,7 +72,6 @@ iimg8 = ImageChops.difference(iimg3, iimg2)
 
 # <codecell>
 
-iimg8.show()
 
 # <codecell>
 
@@ -115,11 +83,9 @@ iimg9swap = iimg8.rotate(180)
 
 # <codecell>
 
-iimg9swap.show()
 
 # <codecell>
 
-iimg3.show()
 
 # <codecell>
 
@@ -131,11 +97,9 @@ img6 = ImageChops.blend(iimg5, iimg, .5)
 
 # <codecell>
 
-img6.show()
 
 # <codecell>
 
-iimg5.show()
 
 # <codecell>
 
@@ -143,7 +107,6 @@ iimgz7 = ImageChops.darker(iimg5, iimg9swap)
 
 # <codecell>
 
-iimgz7.show()
 
 # <codecell>
 
@@ -151,7 +114,6 @@ iimgz8 = iimgz7.filter(ImageFilter.DETAIL)
 
 # <codecell>
 
-iimgz8.show()
 
 # <codecell>
 
@@ -159,23 +121,11 @@ iimgz2 = ImageChops.blend(iimg5, iimg3, .2)
 
 # <codecell>
 
-iimgz2.show()
-
-# <codecell>
-
 imgz10 = iimgz2.filter(ImageFilter.CONTOUR)
 
 # <codecell>
 
-imgz10.show()
-
-# <codecell>
-
 InImgz10 = ImageChops.invert(imgz10)
-
-# <codecell>
-
-InImgz10.show()
 
 # <codecell>
 
@@ -196,33 +146,7 @@ blenzImg = ImageChops.difference(dieFix, iimg8)
 
 # <codecell>
 
-blenzImg.show()
-
-# <codecell>
-
 randNumz = random.randint(1,10)
-
-# <codecell>
-
-
-# <codecell>
-
-os.chdir('/home/will/Desktop/cook')
-
-# <codecell>
-
-showCook = Image.open(imagCook)
-
-# <codecell>
-
-from PIL import ImageFont
-
-# <codecell>
-
-
-# <codecell>
-
-import sys
 
 # <codecell>
 
@@ -236,51 +160,7 @@ iimg5.save(imagRanxz)
 
 # <codecell>
 
-
-# <codecell>
-
-
-# <codecell>
-
-
-# <codecell>
-
-
-# <codecell>
-
-
-# <codecell>
-
-
-# <codecell>
-
-resize = (1920, 1080)
-
-# <codecell>
-
-
-# <codecell>
-
-
-# <codecell>
-
-
-# <codecell>
-
-largeImg3.format, img3other.size, img3other.mode
-
-# <headingcell level=1>
-
-# hello there 
-
-# <markdowncell>
-
-# this is a test 
-
-# <codecell>
-
 img7 = ImageChops.invert(img6)
-img7.show()
 
 # <codecell>
 
@@ -290,7 +170,6 @@ img7.save(imagRandz)
 
 enchane = ImageEnhance.Brightness(iimg)
 blendz = enchane.enhance(2)
-blendz.show()
 
 # <codecell>
 
@@ -298,217 +177,7 @@ bluzImage = ImageChops.blend(iimgz2, iimg5, .3)
 
 # <codecell>
 
-bluzImage.show()
-
-# <codecell>
-
 print randNumz
-
-# <headingcell level=5>
-
-# Create ward room numbers
-
-# <codecell>
-
-wardRooms = range(1,11)
-
-# <codecell>
-
-print wardRooms
-
-# <codecell>
-
-wardRooms.append('b')
-
-# <codecell>
-
-print wardRooms
-
-# <codecell>
-
-for data in enumerate(wardRooms):
-    print data
-
-# <codecell>
-
-for info in wardRooms:
-    print info
-
-# <codecell>
-
-wardRooms * 4
-
-# <codecell>
-
-startWard = range(1,13)
-
-# <codecell>
-
-print startWard
-
-# <codecell>
-
-startWard.extend('a')
-
-# <codecell>
-
-print startWard
-
-# <codecell>
-
-startWard * 4
-
-# <codecell>
-
-roomLet = ['a','b','c','d']
-
-# <codecell>
-
-print 'a' * 12
-
-# <codecell>
-
-print roomLet
-
-# <codecell>
-
-roomLet * 12
-
-# <codecell>
-
-startWard.append(roomLet)
-
-# <codecell>
-
-print startWard
-
-# <codecell>
-
-roomZero =  startWard[0]
-
-# <codecell>
-
-myStr = str(roomZero)
-
-# <codecell>
-
-roomLetter = printroom[0]
-
-# <codecell>
-
-roomStr = str(roomLetter)
-
-# <codecell>
-
-finalNum = myStr + roomStr
-
-# <codecell>
-
-
-# <codecell>
-
-SatBreak = ['wheetbix', 'cornflakes', 'porridge']
-MilkType = ['blue', 'light', 'soy']
-BreadType = ['wholemeal', 'white']
-BreadToast = [True, False]
-HaveButter = [True, False]
-SpreadType  = ['peanut butter', 'jam', 'honey']
-NumToast = [0,1,2,3]
-
-# <codecell>
-
-print NumToast
-
-# <codecell>
-
-import random
-
-# <codecell>
-
-dry = random.choice(SatBreak)
-
-# <codecell>
-
-dry
-
-# <codecell>
-
-milk = random.choice(MilkType)
-
-# <codecell>
-
-print milk
-
-# <codecell>
-
-bType = random.choice(BreadType)
-
-# <codecell>
-
-print bType
-
-# <codecell>
-
-NumbToas = random.randint(1,3)
-
-# <codecell>
-
-print NumbToas
-
-# <codecell>
-
-class MenuItems():
-    def sayHello():
-        print ('hello')
-
-# <codecell>
-
-MenuItems.sayHello()
-
-# <headingcell level=1>
-
-#  I HATE SPORT
-
-# <codecell>
-
-spread = random.choice(SpreadType)
-
-# <codecell>
-
-spread
-
-# <codecell>
-
-BreakLast = []
-
-# <codecell>
-
-BreakLast.append(dry)
-
-# <codecell>
-
-BreakLast.append(milk)
-
-# <codecell>
-
-BreakLast.append(bType)
-
-# <codecell>
-
-BreakLast.append(spread)
-
-# <codecell>
-
-print BreakLast
-
-# <codecell>
-
-
-# <codecell>
-
-
-# <codecell>
-
 
 # <codecell>
 
@@ -524,15 +193,7 @@ mixCas = Image.open(cassie)
 
 # <codecell>
 
-mixCas.show()
-
-# <codecell>
-
 mixCok = ImageChops.blend(iimg, mixCas, .5)
-
-# <codecell>
-
-mixCok.show()
 
 # <codecell>
 
@@ -548,6 +209,10 @@ cassie2 = random.choice(os.listdir('/home/will/Desktop/arcticMonkeys'))
 
 # <codecell>
 
+cassie3 = random.choice(os.listdir('/home/will/Desktop/arcticMonkeys'))
+
+# <codecell>
+
 arcImage = Image.open(cassie)
 
 # <codecell>
@@ -556,246 +221,34 @@ arcImage2 = Image.open(cassie2)
 
 # <codecell>
 
+arcImage3 = Image.open(cassie3)
+
+# <codecell>
+
+os.chdir('/home/will/Desktop/arcOut/')
+
+# <codecell>
+
 arcSwap = ImageChops.blend(arcImage, arcImage2, .5)
 
 # <codecell>
 
-arcSwap.show()
+
+arcSwap.save(cassie)
 
 # <codecell>
 
 arcBlend = ImageChops.darker(arcSwap, arcImage2)
-arcBlend.show()
+arcBlend.save(cassie2)
 
 # <codecell>
 
-arcBlur = arcBlend.filter(ImageFilter.BLUR)
-arcBlur.show()
-
-# <codecell>
-
-import json
-
-# <codecell>
-
-wirePil = open('wirePIL.ipynb')
-
-# <codecell>
-
-import PIL
-
-# <markdowncell>
-
-# How can I read a ipynb as a JSON object like I do normal JSON objects?
-
-# <codecell>
-
-wirePil(0,80)
-
-# <codecell>
-
-PIL.ImageFilter.FIND_EDGES()
-
-# <codecell>
-
-PIL.ImageFilter.BLUR()
-
-# <codecell>
-
-MenuSize = ['small', 'med', 'large']
-
-# <codecell>
-
-optionOne = ['chicken stir-fry', 'beef lasagne', 'vegetable lasagne']
-
-# <codecell>
-
-mainMeal = random.choice(optionOne)
-
-# <codecell>
-
-mashedPotato = [True, False]
-
-# <codecell>
-
-seasonVege = [True, False]
-
-# <codecell>
-
-salt = [True, False]
-
-# <codecell>
-
-saltChoice = random.choice(salt)
-
-# <codecell>
-
-print saltChoice
-
-# <codecell>
-
-random.choice(salt)
-
-# <codecell>
-
-peper = [True, False]
-
-# <codecell>
-
-pepChoice = random.choice(peper)
-
-# <codecell>
-
-puding = ['Fruit ShortCake', 'Melrose Cream', 'Stewed Fruit', 'Jelly & Ice Cream']
-
-# <codecell>
-
-pudChoice = random.choice(puding)
-
-# <codecell>
-
-print pudChoice
-
-# <codecell>
-
-dinerList = []
-
-# <codecell>
-
-dinerList.append('salt: ')
-
-# <codecell>
-
-dinerList.append(saltChoice)
-
-# <codecell>
-
-dinerList
-
-# <codecell>
-
-dinerList.append('pepper: ')
-
-# <codecell>
-
-dinerList.append(pepChoice)
+arcCompo = ImageChops.blend(arcImage2, arcImage3, .8)
 
 # <codecell>
 
 
-# <codecell>
-
-
-# <codecell>
-
-dinerList.append('Pudding: ')
-
-# <codecell>
-
-dinerList.append(pudChoice)
-
-# <codecell>
-
-dinerList
-
-# <codecell>
-
-dinerList.append('Dinner: ')
-
-# <codecell>
-
-dinerList.append(mainMeal)
-
-# <codecell>
-
-dinerList
-
-# <codecell>
-
-
-# <codecell>
-
-dinerList[0]
-
-# <codecell>
-
-patDetail = []
-
-# <codecell>
-
-LastName = ['Mckee', 'Harrison', 'Boyld']
-
-# <codecell>
-
-ward = ['er', 'burns', 'plastic', 'child', 'medical']
-
-# <codecell>
-
-print ward
-
-# <codecell>
-
-ranWard = random.choice(ward)
-
-# <codecell>
-
-print ranWard
-
-# <codecell>
-
-cubeNum = random.randint(1,11)
-
-# <codecell>
-
-cubeNum
-
-# <codecell>
-
-cubeLet = ['a','b','c','d']
-
-# <codecell>
-
-ranCube = random.choice(cubeLet)
-
-# <codecell>
-
-print ranCube
-
-# <codecell>
-
-cubeNum 
-
-# <codecell>
-
-x = str(cubeNum)
-
-# <codecell>
-
-y = str(ranCube)
-
-# <codecell>
-
-roomNumb = x + y
-
-# <codecell>
-
-print roomNumb
-
-# <codecell>
-
-
-# <codecell>
-
-patDetail.append
-
-# <codecell>
-
-
-# <codecell>
-
-
-# <codecell>
-
+arcCompo.save(cassie3)
 
 # <codecell>
 
